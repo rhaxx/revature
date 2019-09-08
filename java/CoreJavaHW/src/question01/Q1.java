59be45172870616fa831851a87c602f39379c9db
Ambiorix Cruz Angeles <cruzangeles.ambiorix@gmail.com>
2019-09-07 23:23:44 -0400

    completed first question

---
+++ java/CoreJavaHW/src/question01/Q1.java
@@ -0,0 +1,33 @@
+package question01;
+
+/*
+ * Q1. Perform a bubble sort on the following integer array: 1,0,5,6,3,2,3,7,9,8,4 
+ */
+
+public class Q1 {
+
+	public static void main(String[] args) {
+		int[] arr1 = {1,0,5,6,3,2,3,7,9,8,4};
+		for(int i = 0; i < arr1.length; i++) System.out.print(arr1[i] + " ");
+		System.out.println();
+		int[] arr2 = bubbleSort(arr1);
+		for(int i = 0; i < arr2.length; i++) System.out.print(arr2[i] + " ");
+	}
+	
+	static int[] bubbleSort(int[] arr) {
+		for(int j = 0; j < arr.length; j++) {
+			for(int i = 0; i < arr.length-1; i++) {
+				if(arr[i] < arr[i+1]) {
+					continue;
+				}
+				else {
+					int temp = arr[i];
+					arr[i] = arr[i+1];
+					arr[i+1] = temp;
+				}
+			}
+		}
+		return arr;
+	}
+	
+}
